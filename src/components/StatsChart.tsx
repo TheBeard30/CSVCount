cfb530c118bc85c95f2de18060918423d22d0832
李豪珣 <519367854@qq.com>
2025-08-13 00:16:12 +0800

    Update bar chart to use light blue gradient colors

--- src/components/StatsChart.tsx
+++ src/components/StatsChart.tsx
@@ -73,8 +73,8 @@ export const StatsChart: React.FC<StatsChartProps> = ({ report, activeField }) =
       <BarChart data={chartData} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
         <defs>
           <linearGradient id="barGradient" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="0%" stopColor="#8884d8" stopOpacity={0.8} />
-            <stop offset="100%" stopColor="#8884d8" stopOpacity={0.1} />
+            <stop offset="0%" stopColor="#93c5fd" stopOpacity={0.8} />
+            <stop offset="100%" stopColor="#93c5fd" stopOpacity={0} />
           </linearGradient>
         </defs>
         <CartesianGrid strokeDasharray="3 3" />
